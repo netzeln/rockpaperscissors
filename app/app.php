e182eae99aba3343697c51e9c933d6bc6fdab862
Jason Awbrey and nic netzel <jason.s.awbrey@gmail.com>
2016-02-17 09:31:12 -0800

    Add functional UI, MVP met

--- app/app.php
+++ app/app.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
 
-	require_once __DIR__.'/../vendor/autoload.php'; 
+	require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/../src/RockPaperScissors.php';
 
-	$app = new Silex\Application(); 
 
-	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views')); 
+	$app = new Silex\Application();
 
-	$app->get('/', function(){return 'Hello, World!';}); 
+	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	return $app; 
+	$app->get('/', function() use ($app){
+		return $app['twig']->render('rps.html.twig');
+	});
+
+	$app->get('/play', function() use ($app){
+		$rps = new RockPaperScissors($_GET['player1'], $_GET['player2']);
+		return $app['twig']->render('rps.html.twig', array('rps' => $rps));
+	});
+
+	return $app;
 
 ?>
